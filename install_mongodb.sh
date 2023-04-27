e4ff568ba541e5939aecbbb4f167677cdcbcb9e5
artemsmirnov <artemsmirnov@MacBook-Air-Artem.local>
2023-04-27 16:53:27 +0300

    HW-06

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 apt-get install apt-transport-https ca-certificates
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+#wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb
 sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo systemctl status mongod | grep active
