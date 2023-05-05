42dbfad09b2e02ab72248edb4e41e1a4c62726d5
artemsmirnov <artemsmirnov@MacBook-Air-Artem.local>
2023-05-05 20:15:12 +0300

    terraform-2

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -6,7 +6,6 @@ terraform {
     key        = "terraform.tfstate"
     access_key = "YCAu7W"
     secret_key = "YCM8Rv"
-
     skip_region_validation      = true
     skip_credentials_validation = true
   }
